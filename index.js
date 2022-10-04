e4aa41c5d1bea117422ddc93451facfa85108a12
Kyle Goodall <k.goodall5@gmail.com>
2022-10-04 09:25:43 +0400

    added player input

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 //Function for computer selection. 
 function getComputerChoice (){
-    let choice = ['Rock', 'Paper', 'Scissors']
+    let choice = ['rock', 'paper', 'scissors']
     let selection = choice[Math.floor(Math.random()*choice.length)];
     alert ('The computer chose: ' + selection);
     return selection;
@@ -11,24 +11,31 @@ function getComputerChoice (){
 
 function playRound(playerSelection, computerSelection){ if (playerSelection === computerSelection){
     return 'It\'s a draw!'
-} else if ( playerSelection === 'Rock' & computerSelection === 'Scissors') {
+} else if ( playerSelection === 'rock' & computerSelection === 'scissors') {
     return 'You win! Rock beats scissors'
-} else if ( playerSelection === 'Rock' & computerSelection === 'Paper') {
+} else if ( playerSelection === 'rock' & computerSelection === 'paper') {
     return 'You lose! Paper beats rock'
-} else if (playerSelection === 'Paper' & computerSelection === 'Rock') {
+} else if (playerSelection === 'paper' & computerSelection === 'rock') {
     return 'You win! Paper beats rock'
-} else if ( playerSelection === 'Paper' & computerSelection === 'Scissors' ) {
+} else if ( playerSelection === 'paper' & computerSelection === 'scissors' ) {
     return 'You lose! Scissors beats Paper'
-} else if ( playerSelection === 'Scissors' & computerSelection === 'Paper') {
+} else if ( playerSelection === 'scissors' & computerSelection === 'paper') {
     return 'You win! Scissors beats Paper'
-} else if ( playerSelection === 'Scissors' & computerSelection === 'Rock') {
+} else if ( playerSelection === 'scissors' & computerSelection === 'rock') {
     return 'You lose! Rock beats Scissors'
 }
 }
 
-const playerSelection = 'Rock'
+//function for player input
+function getPlayerChoice(){
+   let playerChoice = prompt ('Please enter either Rock, Paper or Scissors', '');
+   choiceLower = playerChoice.toLowerCase();
+   alert ('You chose ' + choiceLower);
+   return choiceLower
+}
+
+
+const playerSelection = getPlayerChoice()
 const computerSelection = getComputerChoice()
 
 console.log(playRound(playerSelection, computerSelection))
-
-//function for player input
